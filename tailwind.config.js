c44a92e3b1ceb223a6b10ab98a1ad0216e967127
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-10 22:28:19 +0530

    add custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      mint: "#cbf4f3",
+      blue: "#032b44",
+      mintExtreme: "#4BDFEC",
+    },
+    fontFamily: {
+      primary: ["Neue Regrade", "sans-serif"],
+      secondary: ["Gantari", "sans-serif"],
+    },
   },
   plugins: [],
-}
+};
