fc60baba47969d0af0a6f1b3f52333663de297c2
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-11 19:11:16 +0530

    configured react router

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,30 @@ import App from "./App.jsx";
 import { store } from "./app/store/store.js";
 import { Provider } from "react-redux";
 import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import { Signup, Signin } from "./pages/index.js";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
+    <RouterProvider router={router}>
       <App />
-    </Provider>
+    </RouterProvider>
+  </Provider>
   // {/* </React.StrictMode> */}
 );
