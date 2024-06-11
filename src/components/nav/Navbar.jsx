0eeaefd9932348fb4d1ecee2eaf08898e5fb6810
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-11 21:28:01 +0530

    add links

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "../buttons/Button";
 import { buttonTypes } from "../../utils";
 import { navLinks } from "../../utils/navLinks";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -9,7 +10,7 @@ const Navbar = () => {
       <div className="flex items-center gap-20">
         <div>
           <h1 className="font-primary text-[2.2rem] font-semibold py-5 text-mint">
-            <a href="/">Portify</a>
+            <Link to="/">Portify</Link>
           </h1>
         </div>
 
@@ -28,7 +29,9 @@ const Navbar = () => {
       </div>
 
       <div>
-        <Button type={buttonTypes.PRIMARY}>Register</Button>
+        <Link to={"/signup"}>
+          <Button type={buttonTypes.PRIMARY}>Register</Button>
+        </Link>
       </div>
     </div>
   );
