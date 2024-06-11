df75b1af5b4d153d600a5a4d4f44485695df5a77
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-11 19:11:25 +0530

    add pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Editor from "./editor/Editor";
 import Landing from "./landing/Landing";
+import EmailVerification from "./authentication/EmailVerification";
+import Signup from "./authentication/Signup";
+import Signin from "./authentication/Signin";
 
-export{
-    Editor,
-    Landing,
-}
+export { Editor, Landing, EmailVerification, Signin, Signup, };
