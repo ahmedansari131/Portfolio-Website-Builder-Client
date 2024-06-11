6bd4ee38959fda526d9f622e42299e0ba43f5232
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-11 21:44:23 +0530

    add input field component

---
+++ src/components/input/InputField.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { cn } from "../../utils";
+
+const InputField = (props, ref) => {
+  const { type, label, placeholder, inputClassName, labelClassName } = props;
+  return (
+    <div className="flex flex-col">
+      {label && (
+        <label className={cn(`text-lg`, labelClassName)} htmlFor="">
+          {label}
+        </label>
+      )}
+      <input
+        ref={ref}
+        className={cn(
+          `px-4 py-2 text-white outline-none border border-mintExtreme border-opacity-20 focus:border-opacity-55 hover:bg-opacity-15 transition-all duration-200 rounded-md bg-mintExtreme bg-opacity-10 placeholder:text-sm placeholder:text-mint placeholder:text-opacity-45`,
+          inputClassName
+        )}
+        type={type}
+        placeholder={placeholder}
+        {...props}
+      />
+    </div>
+  );
+};
+
+export default React.forwardRef(InputField);
