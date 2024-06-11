a065609665f7472c0bf81da372c1b4ff7a6ff6cf
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-11 22:42:08 +0530

    configure rtk query

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { editorSlice, dropdownSlice } from '../slices'
+import { editorSlice, dropdownSlice } from "../slices";
+import { authApi } from "../../services/api/authApi";
 
 export const store = configureStore({
-    reducer: {
-        editor: editorSlice,
-        dropdown: dropdownSlice,
-    },
+  reducer: {
+    [authApi.reducerPath]: authApi.reducer,
+    editor: editorSlice,
+    dropdown: dropdownSlice,
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
 });
