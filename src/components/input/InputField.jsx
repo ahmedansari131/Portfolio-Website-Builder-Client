7bef0b2094d8fe4762f413d5e93c94ebf53d75b2
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-13 22:19:25 +0530

    style: add font

--- src/components/input/InputField.jsx
+++ src/components/input/InputField.jsx
@@ -13,7 +13,7 @@ const InputField = (props, ref) => {
       <input
         ref={ref}
         className={cn(
-          `px-4 py-2 text-white outline-none border border-mintExtreme border-opacity-20 focus:border-opacity-55 hover:bg-opacity-15 transition-all duration-200 rounded-md bg-mintExtreme bg-opacity-10 placeholder:text-sm placeholder:text-mint placeholder:text-opacity-45`,
+          `px-4 py-2 text-white outline-none border border-mintExtreme border-opacity-20 focus:border-opacity-55 hover:bg-opacity-15 transition-all duration-200 rounded-md bg-mintExtreme bg-opacity-10 placeholder:text-sm placeholder:text-mint placeholder:text-opacity-45 font-secondary`,
           inputClassName
         )}
         type={type}
