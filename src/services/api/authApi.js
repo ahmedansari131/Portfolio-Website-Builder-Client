fe115c726f5caffab145bb9a849e1f4040aae7b1
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-13 22:17:40 +0530

    add api endpoint for email verification

--- src/services/api/authApi.js
+++ src/services/api/authApi.js
@@ -2,13 +2,21 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const authApi = createApi({
   reducerPath: "authApi",
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_PATH_PREFIX }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${import.meta.env.VITE_API_PATH_PREFIX}/auth/`,
+  }),
   endpoints: (builder) => ({
     registerUser: builder.mutation({
       query: (data) => ({
         url: "register/",
         method: "POST",
         body: data,
+      }),
+    }),
+    verifyEmail: builder.query({
+      query: (token) => ({
+        url: `verify-email/?token=${token}`,
+        method: "GET",
       }),
     }),
     loginUser: builder.mutation({
@@ -21,4 +29,4 @@ export const authApi = createApi({
   }),
 });
 
-export const { useRegisterUserMutation, userLoginUserMutation } = authApi;
+export const { useRegisterUserMutation, userLoginUserMutation, useVerifyEmailQuery } = authApi;
