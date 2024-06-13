67be90c0229c70e616e3d99f9acd815812393a6c
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-13 22:19:40 +0530

    refactor: add props

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -3,40 +3,41 @@ import { cn } from "../../utils";
 import { buttonTypes } from "../../utils";
 
 const Button = (props) => {
-  const { type, children, className = "" } = props;
-  let buttonStyles = " ";
-  switch (type) {
+  const { buttonType, children, handler=null, className = "" } = props;
+  let buttonStyles = "";
+  switch (buttonType) {
     case buttonTypes.PRIMARY:
-      buttonStyles += "bg-gradient";
+      buttonStyles = "bg-gradient";
       break;
 
     case buttonTypes.SECONDARY:
-      buttonStyles +=
-        "bg-mint border-none text-blue font-semibold";
+      buttonStyles = "bg-mint border-none text-blue font-semibold";
       break;
 
     case buttonTypes.TERTIARY:
-      buttonStyles +=
+      buttonStyles =
         "bg-transparent text-teal-600 border-none hover:text-teal-700";
       break;
 
     case buttonTypes.SPECIAL:
-      buttonStyles += "bg-gradient-special";
+      buttonStyles = "bg-gradient-special";
       break;
 
     default:
       break;
   }
-
   return (
-    <button
-      className={cn(
-        `${buttonStyles} font-secondary px-6 py-2 rounded-md text-mint text-lg border border-mintExtreme border-opacity-40 hover:bg-mintExtreme hover:bg-opacity-30 transition-all duration-200 active:bg-opacity-60`,
-        className
-      )}
-    >
-      {children}
-    </button>
+    <div>
+      <button
+        className={cn(
+          `${buttonStyles} font-secondary px-6 py-2 rounded-md text-mint text-lg border border-mintExtreme border-opacity-40 hover:bg-mintExtreme hover:bg-opacity-30 transition-all duration-200 active:bg-opacity-60`,
+          className
+        )}
+        onClick={handler}
+      >
+        {children}
+      </button>
+    </div>
   );
 };
 
