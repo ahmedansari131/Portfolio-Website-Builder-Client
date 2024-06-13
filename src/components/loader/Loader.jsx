280e3e55ca3c04f543c577ae0947369f19f31359
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-13 22:19:09 +0530

    add loader component

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="padding-inherit">
+      <div class="dot-carousel"></div>
+    </div>
+  );
+};
+
+export default Loader;
