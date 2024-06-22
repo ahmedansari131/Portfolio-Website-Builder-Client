447bb3c5719a0d085b438820ac45e8e0d4971a13
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-22 16:22:28 +0530

    add user slice

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { editorSlice, dropdownSlice } from "../slices";
 import { authApi } from "../../services/api/authApi";
+import userSlice from "../slices/authentication/userSlice";
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
+    user: userSlice,
     editor: editorSlice,
     dropdown: dropdownSlice,
   },
