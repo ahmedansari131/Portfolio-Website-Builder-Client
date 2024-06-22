3b98d5be1a8e8ae0afd1766cf2596de359e4ba0e
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-23 02:01:35 +0530

    adjust loader

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { cn } from "../../utils";
 
-const Loader = () => {
-  return (
-    <div className="padding-inherit">
-      <div className="dot-carousel"></div>
-    </div>
-  );
+const Loader = ({ className }) => {
+  const dots = 5;
+  const dotElements = Array.from({ length: dots }, (_, index) => (
+    <div
+      key={index}
+      className={cn(`newtons-cradle__dot bg-blue rounded-full`, className)}
+    ></div>
+  ));
+  return <div className={`newtons-cradle padding-inherit`}>{dotElements}</div>;
 };
 
 export default Loader;
