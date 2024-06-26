fc96d9c8844d007dcc08a56e3ff1000c93631801
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-26 21:36:02 +0530

    style: add width 100%

--- src/components/input/InputField.jsx
+++ src/components/input/InputField.jsx
@@ -4,7 +4,7 @@ import { cn } from "../../utils";
 const InputField = (props, ref) => {
   const { type, label, placeholder, className, labelClassName, children, ...inputProps } =
     props;
-
+    
   return (
     <div className="flex flex-col relative w-full">
       {label && (
@@ -15,7 +15,7 @@ const InputField = (props, ref) => {
       <input
         ref={ref}
         className={cn(
-          `px-4 py-2 text-white outline-none border border-mintExtreme border-opacity-20 focus:border-opacity-55 hover:bg-opacity-15 transition-all duration-200 rounded-md bg-mintExtreme bg-opacity-10 placeholder:text-sm placeholder:text-mint placeholder:text-opacity-45 font-secondary`,
+          `px-4 py-2 text-white outline-none border border-mintExtreme border-opacity-20 focus:border-opacity-55 hover:bg-opacity-15 transition-all duration-200 rounded-md bg-mintExtreme bg-opacity-10 placeholder:text-sm placeholder:text-mint placeholder:text-opacity-45 font-secondary w-full`,
           className
         )}
         type={type}
