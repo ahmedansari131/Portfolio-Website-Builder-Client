2a18342ca0e88918e147eeb46b3fc71391c08e6b
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-27 23:36:18 +0530

    add change pofile image page

---
+++ src/components/account/ChangeProfileImage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChangeProfileImage = () => {
+  return (
+    <div className="rounded-lg border border-light bg-mintExtreme bg-opacity-10 text-mint px-8 py-4 w-full">
+      Change Profile Image
+    </div>
+  );
+};
+
+export default ChangeProfileImage;
