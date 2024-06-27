1796683c8ae0bb486011145d8e94289ad078ffe4
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-06-27 23:35:59 +0530

    add routes

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import {
 } from "react-router-dom";
 import Layout from "./Layout.jsx";
 import {
-  AccountSetting,
+  Settings,
   DirectSignin,
   Editor,
   Landing,
@@ -19,29 +19,38 @@ import {
   Signin,
   Signup,
   VerifyEmail,
+  Dashboard,
+  Account,
+  PortfolioProjects,
 } from "./pages/index.js";
 import { ProtectedRoutes } from "./components/index.js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="/" element={<Landing />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/verify-email" element={<VerifyEmail />} />
-      <Route
-        path="/reset-forgot-password/:uid/:token"
-        element={<ResetForgotPassword />}
-      />
-      <Route
-        path="/direct-signin/:uid/:signin_token"
-        element={<DirectSignin />}
-      />
-      <Route element={<ProtectedRoutes />}>
-        <Route path="/editor" element={<Editor />} />
-        <Route path="/account-setting" element={<AccountSetting />} />
+    <>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Landing />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route
+          path="/reset-forgot-password/:uid/:token"
+          element={<ResetForgotPassword />}
+        />
+        <Route
+          path="/direct-signin/:uid/:signin_token"
+          element={<DirectSignin />}
+        />
       </Route>
-    </Route>
+      <Route path="settings" element={<Settings />}>
+        <Route element={<ProtectedRoutes />}>
+          <Route path="editor" element={<Editor />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="account" element={<Account />} />
+          <Route path="portfolio-projects" element={<PortfolioProjects />} />
+        </Route>
+      </Route>
+    </>
   )
 );
 
